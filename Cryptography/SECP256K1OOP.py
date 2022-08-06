0f0a924bcd5c6128a55dd49eca2a416f79067e03
MaltoonYezi <76572326+MaltoonYezi@users.noreply.github.com>
2022-08-06 13:27:51 +0300

    Secp256k1 OOP implementation (Programming Bitcoin)
    Private to Public key conversion implementation from "Programming Bitcoin" book by Jimmy Song. 
    I've refactored it a bit for personal needs

---
+++ Cryptography/SECP256K1OOP.py
@@ -0,0 +1,194 @@
+
+class FieldElement:
+
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = 'Num {} not in field range 0 to {}'.format(
+                num, prime - 1)
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self):
+        return 'FieldElement_{}({})'.format(self.prime, self.num)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        return not (self == other)
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num + other.num) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot subtract two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num - other.num) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __mul__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot multiply two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num * other.num) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        # use fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+        num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __rmul__(self, coefficient):
+        num = (self.num * coefficient) % self.prime
+        return self.__class__(num=num, prime=self.prime)
+
+class Point:
+
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        if self.x is None and self.y is None:
+            return
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+    # end::source1[]
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
+
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        return not (self == other)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        elif isinstance(self.x, FieldElement):
+            return 'Point({},{})_{}_{} FieldElement({})'.format(
+                self.x.num, self.y.num, self.a.num, self.b.num, self.x.prime)
+        else:
+            return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
+        # Case 0.0: self is the point at infinity, return other
+        if self.x is None:
+            return other
+        # Case 0.1: other is the point at infinity, return self
+        if other.x is None:
+            return self
+
+        # Case 1: self.x == other.x, self.y != other.y
+        # Result is point at infinity
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+
+        # Case 2: self.x ≠ other.x
+        # Formula (x3,y3)==(x1,y1)+(x2,y2); S is the slope
+        # s=(y2-y1)/(x2-x1)
+        # x3=s**2-x1-x2
+        # y3=s*(x1-x3)-y1
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+        # Case 4: if we are tangent to the vertical line,
+        # we return the point at infinity
+        # note instead of figuring out what 0 is for each type
+        # we just use 0 * self.x
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+
+        # Case 3: self == other
+        # Formula (x3,y3)=(x1,y1)+(x1,y1)
+        # s=(3*x1**2+a)/(2*y1)
+        # x3=s**2-2*x1
+        # y3=s*(x1-x3)-y1
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x = s**2 - 2 * self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+    # tag::source3[] BINARY EXPANSION MULTIPLICATION
+    def __rmul__(self, coefficient):
+        coef = coefficient
+        current = self  # <1>
+        result = self.__class__(None, None, self.a, self.b)  # <2>
+        while coef:
+            if coef & 1:  # <3>
+                result += current
+            current += current  # <4>
+            coef >>= 1  # <5>
+        return result
+    # end::source3[]
+
+#Order of the finite field
+p = 2**256 - 2**32 - 977
+#G coordinates
+gx = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+#Order of the group G
+n = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+#n -1 => is the number of all possible private keys. This is an example og a biggest possible one
+privateKey = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364140
+
+x = FieldElement(gx, p)
+y = FieldElement(gy, p)
+b = FieldElement(7, p)
+a = FieldElement(0, p)
+G = Point(x, y, a, b)
+
+#Testing if the G coordinates are right
+print("Are Gcoordinates right?:",gy**2%p==(gx**3+7)%p)
+#Testing the infinity point
+print(n*G)
+
+#Testing
+#Case 1. priv is a private key
+priv = 0x45300f2b990d332c0ee0efd69f2c21c323d0e2d20e7bfa7b1970bbf169174c82
+#Right public key coordinates:
+#x = 40766947848522619068424335498612406856128862642075168802372109289834906557916
+#y = 70486353993054234343658342414815626812704078223802622900411169732153437188990
+print("Test case 1",priv*G)
+
+#Case 2
+priv = 0x3d7a8e37e4b0bee158aaa1cd59f5fd00687dff9f5856885895de1d9627c79364
+#Right public key coordinates:
+#x = 39423578571032435044709103897011871405170265732490782941797029603869780180697
+#y = 71546655698862778867132453638384448620813925930703281442431374399415200983411
+print("Test case 2",priv*G)

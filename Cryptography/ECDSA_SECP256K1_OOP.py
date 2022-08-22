fe5cc78df686c6cdbc8a1ff9fd5b4da3b933621d
MaltoonYezi <76572326+MaltoonYezi@users.noreply.github.com>
2022-08-22 23:01:17 +0300

    ECDSA SECP256K1
    Object oriented inplementation

---
+++ Cryptography/ECDSA_SECP256K1_OOP.py
@@ -0,0 +1,243 @@
+
+
+class FieldElement:
+
+    def __init__(self, num, prime):
+        if num >= prime or num < 0:
+            error = 'Num {} not in field range 0 to {}'.format(
+                num, prime - 1)
+            raise ValueError(error)
+        self.num = num
+        self.prime = prime
+
+    def __repr__(self):
+        return 'FieldElement_{}({})'.format(self.prime, self.num)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        return not (self == other)
+
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num + other.num) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot subtract two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num - other.num) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __mul__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot multiply two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        num = (self.num * other.num) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different Fields')
+        # self.num and other.num are the actual values
+        # self.prime is what we need to mod against
+        # use fermat's little theorem:
+        # self.num**(p-1) % p == 1
+        # this means:
+        # 1/n == pow(n, p-2, p)
+        num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
+        # We return an element of the same class
+        return self.__class__(num, self.prime)
+
+    def __rmul__(self, coefficient):
+        num = (self.num * coefficient) % self.prime
+        return self.__class__(num=num, prime=self.prime)
+
+class Point:
+
+    def __init__(self, x, y, a, b):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        if self.x is None and self.y is None:
+            return
+        if self.y**2 != self.x**3 + a * x + b:
+            raise ValueError('({}, {}) is not on the curve'.format(x, y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y \
+            and self.a == other.a and self.b == other.b
+
+    def __ne__(self, other):
+        # this should be the inverse of the == operator
+        return not (self == other)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'Point(infinity)'
+        elif isinstance(self.x, FieldElement):
+            return 'Point({},{})_{}_{} FieldElement({})'.format(
+                self.x.num, self.y.num, self.a.num, self.b.num, self.x.prime)
+        else:
+            return 'Point({},{})_{}_{}'.format(self.x, self.y, self.a, self.b)
+
+    def __add__(self, other):
+        if self.a != other.a or self.b != other.b:
+            raise TypeError('Points {}, {} are not on the same curve'.format(self, other))
+        # Case 0.0: self is the point at infinity, return other
+        if self.x is None:
+            return other
+        # Case 0.1: other is the point at infinity, return self
+        if other.x is None:
+            return self
+
+        # Case 1: self.x == other.x, self.y != other.y
+        # Result is point at infinity
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+
+        # Case 2: self.x ≠ other.x
+        # Formula (x3,y3)==(x1,y1)+(x2,y2); S is the slope
+        # s=(y2-y1)/(x2-x1)
+        # x3=s**2-x1-x2
+        # y3=s*(x1-x3)-y1
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x = s**2 - self.x - other.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+        # Case 4: if we are tangent to the vertical line,
+        # we return the point at infinity
+        # note instead of figuring out what 0 is for each type
+        # we just use 0 * self.x
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+
+        # Case 3: self == other
+        # Formula (x3,y3)=(x1,y1)+(x1,y1)
+        # s=(3*x1**2+a)/(2*y1)
+        # x3=s**2-2*x1
+        # y3=s*(x1-x3)-y1
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x = s**2 - 2 * self.x
+            y = s * (self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+    #BINARY EXPANSION MULTIPLICATION
+    def __rmul__(self, coefficient):
+        coef = coefficient
+        current = self
+        result = self.__class__(None, None, self.a, self.b)
+        while coef:
+            if coef & 1:
+                result += current
+            current += current
+            coef >>= 1
+        return result
+
+A = 0
+B = 7
+#Order of the finite field
+P = 2**256 - 2**32 - 977
+#Order of the group
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+
+class S256Field(FieldElement):
+
+    def __init__(self, num, prime=None):
+        super().__init__(num=num, prime=P)
+
+    def __repr__(self):
+        return '{:x}'.format(self.num).zfill(64)
+
+
+class S256Point(Point):
+
+    def __init__(self, x, y, a=None, b=None):
+        a, b = S256Field(A), S256Field(B)
+        if type(x) == int:
+            super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
+        else:
+            super().__init__(x=x, y=y, a=a, b=b)
+
+    def __repr__(self):
+        if self.x is None:
+            return 'S256Point(infinity)'
+        else:
+            return 'S256Point({}, {})'.format(self.x, self.y)
+
+    def __rmul__(self, coefficient):
+        coef = coefficient % N
+        return super().__rmul__(coef)
+
+    def verify(self, z, sig):
+        s_inv = pow(sig.s, N - 2, N)
+        u = z * s_inv % N
+        v = sig.r * s_inv % N
+        total = u * G + v * self
+        return total.x.num == sig.r
+
+#g point generator
+G = S256Point(
+    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
+
+privateKeyE = 0xa665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3
+
+#Hash of the message
+hashZ = 0xbe347331b4d87273e674b30384985c639069f852246e8c128417dbb1ca8ba812
+
+#randomly generated number
+randomNumberK = 0x3f712500013085b1b082ffbfcc43c406cefdc97cd5a0977cf861356ed96a7642
+
+#x and y coordinates of R
+R = randomNumberK * G
+r = R.x.num
+
+#Inverse of randomNumberK
+k_inv = pow(randomNumberK, N-2, N)
+
+s = (hashZ+r*privateKeyE) * k_inv % N
+
+pubKey = privateKeyE*G
+
+#Test case 1
+print("The point:", pubKey)
+print("Uncompressed public key:", "\n04", str(pubKey.x.num), str(pubKey.y.num))
+print("Hash of the message:", hashZ)
+print("The signature:")
+print("r:", r)
+print("s:", s)
+
+#Verification
+
+#Inverse of s
+s_inv = pow(s, N-2, N)
+u = hashZ * s_inv % N
+v = r * s_inv % N
+
+if ((u*G + v*pubKey).x.num == r):
+    print("The signature is valid")
+else:
+    print("The signature is valid")
